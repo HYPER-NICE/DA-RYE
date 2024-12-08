12a55621c2320b7f9a0829832ecb383a45f00874
xiyo <bunny@xiyo.dev>
2024-12-08 19:02:04 +0900

    :art: 카테고리 소프트 삭제시 참조가 있으면 오류 유발하도록 트리거 설정

--- DA-GWAN/src/main/resources/db/migration/V5__create_trigger.sql
+++ DA-GWAN/src/main/resources/db/migration/V5__create_trigger.sql
@@ -1,3 +1,27 @@
+-- =======================
+-- 트리거 생성: 참조가 있는 카테고리 삭제시 에러 발생
+-- =======================
+DELIMITER $$
+
+CREATE TRIGGER prevent_delete_with_children
+    BEFORE UPDATE ON category
+    FOR EACH ROW
+BEGIN
+    IF NEW.deleted_date IS NOT NULL THEN
+        IF EXISTS (
+            SELECT 1
+            FROM category
+            WHERE parent_id = OLD.id AND deleted_date IS NULL
+        ) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Cannot delete category with active subcategories.';
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
+
+
 
 -- =======================
 -- 트리거 생성: 재고 수량 자동 업데이트
