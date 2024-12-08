276324a356bb023f4b4c32a5b8943bef05dfc935
xiyo <bunny@xiyo.dev>
2024-12-08 19:02:20 +0900

    :art: 소프트 삭체 컬럼 추가에 의한 조건 추가

--- DA-GWAN/src/main/java/hyper/darye/dao/MemberDao.java
+++ DA-GWAN/src/main/java/hyper/darye/dao/MemberDao.java
@@ -1,25 +1,30 @@
 package hyper.darye.dao;
 
-import hyper.darye.dto.InsertMemberDto;
 import hyper.darye.model.Member;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface MemberDao {
 
-    @Select("SELECT * FROM member")
+    @Select("SELECT * FROM member WHERE deleted_date IS NULL")
     List<Member> selectAll();
 
-    @Insert("INSERT INTO member (email, name, password, mobile, address) VALUES (#{email}, #{name}, #{password}, #{mobile}, #{address})")
-    void insert(InsertMemberDto member);
+    @Insert("INSERT INTO member (email, name, password, mobile, address) " +
+            "VALUES (#{email}, #{name}, #{password}, #{mobile}, #{address})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(Member member);
 
-    @Select("SELECT * FROM member WHERE id = #{id}")
+    @Select("SELECT * FROM member WHERE id = #{id} AND deleted_date IS NULL")
     Member selectById(Long id);
 
-    @Select("SELECT * FROM member WHERE email = #{email}")
+    @Select("SELECT * FROM member WHERE email = #{email} AND deleted_date IS NULL")
     Member selectByEmail(String email);
+
+    @Update("UPDATE member SET name=#{name}, mobile=#{mobile}, address=#{address} WHERE id = #{id}")
+    void update(Member member);
+
+    @Update("UPDATE member SET deleted_date = CURRENT_TIMESTAMP(6) WHERE id = #{id}")
+    void delete(Long id);
 }
