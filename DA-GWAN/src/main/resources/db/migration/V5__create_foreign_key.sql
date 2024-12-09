cfb76403b7820b9cb715da1a4d94ca6c81cd0578
xiyo <bunny@xiyo.dev>
2024-12-09 18:29:59 +0900

    :truck: 버저닝 때문에 버전 증가 시킴

--- DA-GWAN/src/main/resources/db/migration/V5__create_foreign_key.sql
+++ DA-GWAN/src/main/resources/db/migration/V5__create_foreign_key.sql
@@ -21,3 +21,23 @@ ALTER TABLE member_order_detail
 
 ALTER TABLE member_order_detail
     ADD CONSTRAINT FK_order_detail_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE NO ACTION;
+
+-- =======================
+-- 포인트 관련 외래 키 설정
+-- =======================
+ALTER TABLE member_point
+    ADD CONSTRAINT FK_member_point_member FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE;
+
+ALTER TABLE point_transaction
+    ADD CONSTRAINT FK_point_transaction_member FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE NO ACTION;
+
+ALTER TABLE point_transaction
+    ADD CONSTRAINT FK_point_transaction_status FOREIGN KEY (status_id) REFERENCES point_transaction_status (id) ON DELETE NO ACTION;
+
+ALTER TABLE point_transaction
+    ADD CONSTRAINT FK_point_transaction_order FOREIGN KEY (reference_order_id) REFERENCES member_order (id) ON DELETE SET NULL;
+
+ALTER TABLE percentage_point_policy
+    ADD CONSTRAINT FK_percentage_point_policy_grade FOREIGN KEY (member_grade_id) REFERENCES member_grade (id) ON DELETE CASCADE;
+
+
