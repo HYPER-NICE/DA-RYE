f130d8d668fdc3d0b01e295672fb62a5fae83e08
Eliehoenai <rlaehdalsk@naver.com>
2024-12-11 17:41:01 +0900

    멤버 더미 데이터 업로드

--- DA-GWAN/src/main/resources/db/migration/V6__create_basic_data.sql
+++ DA-GWAN/src/main/resources/db/migration/V6__create_basic_data.sql
@@ -10,6 +10,40 @@ VALUES
     ('다소니', 10000, 49999, 180, '최근 180일 동안 1만원 이상 5만원 미만 구매 고객'),
     ('마루한', 0, 9999, 90, '최근 90일 동안 1만원 미만 구매 고객');
 
+-- 배송지 주소 데이터 삽입
+INSERT INTO delivery_address (delivery_address, delivery_address_line, delivery_address_zip_code, address_type, is_primary_address, is_latest_address, created_date, last_modified_date, deleted_date)
+VALUES
+    ('서울시 강남구 역삼동', '123-45', 12345, '집', TRUE, TRUE, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    ('부산시 해운대구 센텀시티', '678-90', 67890, '회사', FALSE, TRUE, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    ('대구시 수성구 황금동', '101-21', 10121, '친구집', TRUE, FALSE, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    ('서울시 마포구 상암동', '22-12', 2212, '실험실', FALSE, FALSE, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    ('경기도 성남시 분당구', '112-33', 11233, '자택', TRUE, TRUE, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL);
+
+-- 고객 데이터 삽입
+INSERT INTO member (id, name, sex, birthdate, email, password, address, address_line, address_zip_code, delivery_address_id, mobile, landline, grade_id, current_points, total_earned_points, total_redeemed_points, member_status, latest_login_date, created_date, last_modified_date, deleted_date)
+VALUES
+    (1, '김철수', 'M', '1985-05-15', 'kim@example.com', 'password1', '서울시 강남구 역삼동','123-45', '12345', 1, '010-1234-5678', '02-1234-5678', 4, 1000, 2000, 500, 'ACTIVE', '2024-12-01 12:00:00', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (2, '이영희', 'F', '1990-08-25', 'lee@example.com', 'password2', '부산시 해운대구 센텀시티', '678-90', '67890', 2, '010-2345-6789', '051-234-6789', 4, 1500, 3000, 800, 'INACTIVE', '2024-12-02 14:30:00', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (3, '박민수', 'M', '1988-12-03', 'park@example.com', 'password3', '대구시 수성구 황금동', '101-21', '10121', 3, '010-3456-7890', '053-345-7890', 3, 1200, 2500, 600, 'ACTIVE', '2024-12-03 16:00:00', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (4, '정은지', 'F', '1992-04-10', 'jung@example.com', 'password4', '서울시 마포구 상암동', '22-12', '2212', 4, '010-4567-8901', '02-4567-8901', 2, 1300, 2700, 700, 'ACTIVE', '2024-12-04 09:00:00', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (5, '최상훈', 'M', '1995-01-18', 'choi@example.com', 'password5', '경기도 성남시 분당구', '112-33', '11233', 5, '010-5678-9012', '031-567-9012', 1, 1100, 2200, 600, 'INACTIVE', '2024-12-05 10:30:00', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL);
+
+-- 고객 정보 이력 데이터 삽입
+INSERT INTO member_history (member_id, name, email, password, address, mobile, grade_id, created_date, last_modified_date, deleted_date, operation_type, history_created_date)
+VALUES
+    (1, '김철수', 'kim@example.com', 'password1', '서울시 강남구', '010-1234-5678', 1, '2020-01-01 10:00:00', '2020-01-10 15:00:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (1, '김철수', 'kim_updated@example.com', 'password1', '서울시 강남구', '010-9999-8888', 1, '2024-12-01 12:00:00', '2024-12-01 12:30:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (2, '이영희', 'lee@example.com', 'password2', '부산시 해운대구', '010-2345-6789', 2, '2021-02-01 12:00:00', '2021-02-05 13:00:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (2, '이영희', 'lee@example.com', 'password2', '부산시 해운대구', '010-2345-6789', 2, '2021-02-01 12:00:00', '2021-02-05 13:00:00', '2024-12-02 14:30:00', 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (3, '박민수', 'park@example.com', 'password3', '대구시 수성구', '010-3456-7890', 3, '2022-03-01 09:00:00', '2022-03-05 14:00:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6));
+
+-- 고객 등급 이력 데이터 삽입
+INSERT INTO member_grade_policy_history (grade_id, grade_name, min_amount, max_amount, period_days, description, created_date, last_modified_date, deleted_date, operation_type, history_created_date)
+VALUES
+    (1, '나누리', 100000, 999999990, 360, '최근 360일 동안 10만원 이상 구매 고객 (최대 금액 수정)', '2023-01-01 10:00:00', '2024-12-01 12:00:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (2, '멋드리', 50000, 99999, 270, '최근 270일 동안 5만원 이상 10만원 미만 구매 고객 (등급 이름 수정)', '2023-02-01 11:00:00', '2024-12-02 14:00:00', NULL, 'UPDATE', CURRENT_TIMESTAMP(6)),
+    (3, '다소니', 10000, 49999, 180, '최근 180일 동안 1만원 이상 5만원 미만 구매 고객', '2023-03-01 09:00:00', '2023-03-05 16:00:00', '2024-12-03 10:00:00', 'UPDATE', CURRENT_TIMESTAMP(6));
+
 -- 루트 카테고리 삽입
 INSERT INTO category (name, parent_id, screen_order)
 VALUES
@@ -140,12 +174,33 @@ VALUES
     ('DAMAGE_IN_TRANSIT', '배송 중 파손'),
     ('OTHER', '기타 사유');
 
+-- 고객 주문 데이터 삽입
+INSERT INTO member_order_main (
+    member_id, orderer_name, orderer_contact, receiver_name, receiver_contact,
+    delivery_address, delivery_detail_address, tracking_number, delivery_request_note,
+    total_price, order_date, delivery_code_id, order_code_id, created_date, last_modified_date, deleted_date
+) VALUES
+      (1, '홍길동', '010-1234-5678', '김철수', '010-9876-5432', '서울시 강남구 역삼동 123-45', '서울시 강남구 역삼동 123-45-101호', '123456789012', '문 앞에 놓아주세요.', 50000, '2024-12-11 10:00:00', 1, 1, '2024-12-11 10:00:00', '2024-12-11 10:00:00', NULL),
+      (2, '이영희', '010-2345-6789', '박수현', '010-8765-4321', '부산시 해운대구 우동 22-7', '부산시 해운대구 우동 22-7-202호', '987654321098', '배송 전 전화 부탁드립니다.', 120000, '2024-12-11 11:15:00', 2, 2, '2024-12-11 11:15:00', '2024-12-11 11:15:00', NULL),
+      (3, '김민수', '010-3456-7890', '정다은', '010-7654-3210', '대전시 유성구 가정동 1-2', '대전시 유성구 가정동 1-2-301호', '456789123456', '포장 상태 확인 후 수령해주세요.', 75000, '2024-12-11 12:30:00', 3, 3, '2024-12-11 12:30:00', '2024-12-11 12:30:00', NULL),
+      (4, '정수영', '010-4567-8901', '최예진', '010-6543-2109', '인천시 송도동 99-10', '인천시 송도동 99-10-402호', '654321987654', '문 앞에 놓아주세요.', 98000, '2024-12-11 14:45:00', 1, 4, '2024-12-11 14:45:00', '2024-12-11 14:45:00', NULL),
+      (5, '박준호', '010-5678-9012', '서지원', '010-5432-1098', '경기도 수원시 장안구 영화동 44-22', '경기도 수원시 장안구 영화동 44-22-101호', '321654987321', '주문 취소가 가능하면 취소 부탁드립니다.', 64000, '2024-12-11 16:00:00', 2, 5, '2024-12-11 16:00:00', '2024-12-11 16:00:00', NULL);
+
 -- 포인트 거래 코드 기본 데이터 삽입
 INSERT INTO point_transaction_type (name, description)
 VALUES
     ('PENDING', '포인트 적립/사용 대기'),
     ('COMPLETED', '포인트 적립/사용 완료'),
     ('CANCELLED', '포인트 적립/사용 취소');
+
+-- 포인트 거래 이력 데이터 삽입
+INSERT INTO point_history (member_id, amount, point_transaction_type_id, description, reference_order_id, created_date, last_modified_date, deleted_date)
+VALUES
+    (1, 1000, 1, '첫 구매 적립', NULL, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (2, -500, 2, '환불로 인한 포인트 차감', 101, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (3, 2000, 1, '추천인 포인트 적립', NULL, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (4, -100, 3, '포인트 취소 요청', 102, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (5, 1500, 2, '상품 구매로 포인트 차감', 103, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL);
 
 -- 고정 포인트 정책 데이터 삽입
 INSERT INTO fixed_point_policy (policy_name, description)
@@ -215,6 +270,15 @@ SELECT id,
        '서비스 제공을 위해 개인정보 이용에 동의합니다.',
        NOW()
 FROM sub_terms;
+
+-- 회원 약관 동의 더미 데이터 삽입
+INSERT INTO member_terms_agreement (member_id, agreed_terms_version_id, agreed_sub_terms_version_id, agreed_date, last_modified_date, deleted_date)
+VALUES
+    (1, 1, 1, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (2, 1, 1, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (3, 2, 1, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (4, 1, NULL, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL),
+    (5, 2, 1, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), NULL);
 
 -- 상품 데이터 삽입
 INSERT INTO product (
