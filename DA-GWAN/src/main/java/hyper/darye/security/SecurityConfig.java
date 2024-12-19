a34b08982679e6fc30aea5498dcce97c93540726
XIYO <bunny@xiyo.dev>
2024-12-20 00:05:09 +0900

    :sparkles: 사인업, 사인인, 사인아웃

--- DA-GWAN/src/main/java/hyper/darye/security/SecurityConfig.java
+++ DA-GWAN/src/main/java/hyper/darye/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,11 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity // 메서드에서 접근제어 메서드 사용 가능하도록 설정
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())  // CSRF 비활성화 (REST API)
+        http
+//                .csrf(csrf -> csrf.disable())  // CSRF 비활성화 (REST API)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션 사용 안함 (JWT/토큰 기반)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/sign-in", "/api/sign-up").permitAll()  // 인증 없이 접근 가능
