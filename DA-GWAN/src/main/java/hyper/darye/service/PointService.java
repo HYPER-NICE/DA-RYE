d05aa9c1ecabc5a39e5d8ec51814d2dbf1604445
YOUNGGYUJUNG <wjddudrb190@naver.com>
2024-12-19 18:10:43 +0900

    포인트 사용  작성 테스트 코드 완료 수정1

--- DA-GWAN/src/main/java/hyper/darye/service/PointService.java
+++ DA-GWAN/src/main/java/hyper/darye/service/PointService.java
@@ -25,30 +25,6 @@ public class PointService {
     @Autowired
     private PointTransactionTypeMapper pointTransactionTypeMapper;
 
-    /**
-     * 멤버의 사용가능한 포인트를 수정 합니다.
-     * 가감할 포인트 량만 입력하세요.
-     * @param pointTransactionType 포인트가 수정된 이유
-     * @param memberId 포인트가 사용될 사용자
-     * @param orderMainId 포인트가 발생하거나 사용된 주문
-     * @param description 설명
-     * @param amount 포인트 발생이 발생하 주문 금액(현금성 금액만, 포인트 결제값은 제외해주세요)
-     */
-    public void update(PointTransactionType pointTransactionType, Long memberId, Long orderMainId, String description, Integer amount) {
-
-        // 포인트 가감
-        memberMapper.updatePoint(memberId, (int)(amount * 0.01));
-
-        // 트랜잭션 쌓기
-        PointTransaction pt = new PointTransaction();
-        pt.setPointTransactionTypeId(pointTransactionType.getId());
-        pt.setMemberId(memberId);
-        pt.setAmount(amount);
-        pt.setDescription(description);
-        pt.setOrderMainId(orderMainId);
-        ptMapper.insertPointTransaction(pt);
-    }
-
     public void usePoint(PointTransactionType pointTransactionType, Long memberId, Long orderMainId, String description, Integer point){
         // 포인트 사용
         if(point <= 0)
--- DA-GWAN/src/test/java/hyper/darye/service/PointServiceTest.java
+++ DA-GWAN/src/test/java/hyper/darye/service/PointServiceTest.java
@@ -32,33 +32,6 @@ public class PointServiceTest {
 
 
 
-    @Test
-    @DisplayName("포인트가 정상적으로 적립되는 경우")
-    void UpdateTest() {
-        // given
-        // addPoint 변수 세팅
-        Long memberId = 3L;
-        String description = "Test Point Description";
-        Integer amount = 10000;
-
-        PointTransactionType pointTransactionType = new PointTransactionType();
-        pointTransactionType = pttMapper.selectByPrimaryKey(1L);
-
-        OrderMain orderMain = new OrderMain();
-        orderMain.setMemberId(memberId);
-        orderMain.setOrderDate(new Date());
-        omMapper.insert(orderMain);
-
-        int recentPoint = memberMapper.selectMemberById(memberId).getPoint();
-
-        // when
-        pointService.update(pointTransactionType, memberId, orderMain.getId(), description, amount);
-
-        // then
-        Integer point = (int)(amount * 0.01);
-        Integer selectedPoint = memberMapper.selectMemberById(memberId).getPoint();
-        assertEquals(point + recentPoint, selectedPoint);
-    }
 
     @Test
     @DisplayName("포인트 사용 테스트")
