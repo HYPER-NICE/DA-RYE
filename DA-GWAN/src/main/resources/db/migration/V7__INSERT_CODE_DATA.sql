959d920cf6c6476292c2e45c2b9f5ce904030725
xiyo <bunny@xiyo.dev>
2024-12-20 17:10:03 +0900

    :test_tube: 게시판 더미 데이터 최종 추가

--- DA-GWAN/src/main/resources/db/migration/V7__INSERT_CODE_DATA.sql
+++ DA-GWAN/src/main/resources/db/migration/V7__INSERT_CODE_DATA.sql
@@ -59,3 +59,20 @@ VALUES ('결제 대기', '결제가 대기 중인 상태'),
        ('환불 대기', '환불이 대기 중인 상태'),
        ('환불 거부', '환불이 거부된 상태'),
        ('환불 완료', '환불이 완료된 상태');
+
+
+-- ==================================================
+-- V6 Board - BOARD
+-- ==================================================
+INSERT INTO BOARD_CATEGORY_CODE (
+    ID, ROOT_CATEGORY, SUB_CATEGORY, ROOT_NAME, ROOT_DESCRIPTION, SUB_NAME, SUB_DESCRIPTION, CREATED_DATE, LAST_MODIFIED_DATE, LAST_MODIFIED_MEMBER, DELETED_DATE
+) VALUES
+      (1, 1, 1, '공지사항', '사이트 전반적인 공지사항', '중요', '중요 공지사항입니다.', NOW(), NOW(), NULL, NULL),
+      (2, 1, 2, '공지사항', '사이트 전반적인 공지사항', '일반', '일반 공지사항입니다.', NOW(), NOW(), NULL, NULL),
+      (3, 2, 1, 'FAQ', '자주 묻는 질문', '주문/결제', '주문/결제와 관련된 FAQ입니다.', NOW(), NOW(), NULL, NULL),
+      (4, 2, 2, 'FAQ', '자주 묻는 질문', '환불/반품/교환', '환불/반품/교환과 관련된 FAQ입니다.', NOW(), NOW(), NULL, NULL),
+      (5, 2, 3, 'FAQ', '자주 묻는 질문', '배송', '배송과 관련된 FAQ입니다.', NOW(), NOW(), NULL, NULL),
+      (6, 2, 4, 'FAQ', '자주 묻는 질문', '회원/포인트', '회원/포인트와 관련된 FAQ입니다.', NOW(), NOW(), NULL, NULL),
+      (7, 2, 5, 'FAQ', '자주 묻는 질문', '사이트 이용/기타', '사이트 이용/기타와 관련된 FAQ입니다.', NOW(), NOW(), NULL, NULL),
+      (8, 3, 1, '1대 1 문의', '1대 1 문의 게시판', '미답변', '답변이 되지 않은 1대1 문의입니다.', NOW(), NOW(), NULL, NULL),
+      (9, 3, 2, '1대 1 문의', '1대 1 문의 게시판', '답변완료', '답변이 완료된 1대1 문의입니다.', NOW(), NOW(), NULL, NULL);
--- DA-GWAN/src/main/resources/db/migration/V8__INSERT_APPLICATION_DATA.sql
+++ DA-GWAN/src/main/resources/db/migration/V8__INSERT_APPLICATION_DATA.sql
@@ -374,3 +374,41 @@ INSERT INTO POINT_HISTORY (
       (1, 3, NULL, -30, '미로그인 소멸', NOW()),  -- 회원 1, 소멸된 포인트
       (2, 3, NULL, -50, '장기간 미사용으로 소멸', NOW()),  -- 회원 2, 소멸된 포인트
       (3, 3, NULL, -20, '미로그인 1년 소멸', NOW());  -- 회원 3, 소멸된 포인트
+
+-- ==================================================
+-- BOARD
+-- 의존성
+-- MEMBER
+-- ==================================================
+
+-- 게시판 데이터 삽입
+INSERT INTO BOARD (
+    ID, PARENT_ID, CATEGORY_ID, WRITER_ID, TITLE, CONTENT, FIXED, REG_DATE, CREATED_DATE, LAST_MODIFIED_DATE, LAST_MODIFIED_MEMBER, DELETED_DATE
+) VALUES
+      (1, NULL, 1, 5, '배송 지연 안내입니다.', '과도한 물량으로 인해 배송이 지연될 예정입니다.', TRUE, NOW(), NOW(), NOW(), NULL, NULL),
+      (2, NULL, 2, 5, '용량 변경 안내입니다.', '녹차 상품의 용량이 변경될 예정입니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (3, NULL, 3, 5, 'Q. 사용 가능한 결제 수단에는 무엇이 있나요?', 'A. 현금결제, 카드결제가 가능합니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (4, NULL, 4, 5, 'Q. 환불은 언제까지 가능한가요?', 'A. 주문이 완료된 시점부터 14일 이내에 가능합니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (5, NULL, 5, 5, 'Q. 배송이 될때까지 얼마나 걸리나요?', 'A. 보통 영업일 2~4일 이내에 배송이 완료될 예정입니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (6, NULL, 6, 5, 'Q. 포인트는 얼마부터 사용 가능한가요?', 'A. 금액에 상관없이 사용가능하십니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (7, NULL, 7, 5, 'Q. 탈퇴 후 며칠이내에 재가입이 가능한가요?', 'A. 저희 사이트는 같은 이메일로 재가입이 불가능합니다. 다른 이메일로 가입하여 주시길 바랍니다.', FALSE, NOW(), NOW(), NOW(), NULL, NULL),
+      (8, NULL, 8, 2, 'Q. 환불 문의입니다.', '어제 구입한 상품 환불해주세요.', FALSE, NOW(), NOW(), NOW(), NULL, NULL);
+
+-- ==================================================
+-- BOARD_IMAGE
+-- 의존성
+-- BOARD
+-- ==================================================
+
+-- 게시판 이미지 데이터 삽입
+INSERT INTO BOARD_IMAGE (
+    ID, BOARD_ID, IMAGE, CREATED_DATE, LAST_MODIFIED_DATE, LAST_MODIFIED_MEMBER, DELETED_DATE
+) VALUES
+      (1, 1, ' ', NOW(), NOW(), NULL, NULL),
+      (2, 1, ' ', NOW(), NOW(), NULL, NULL),
+      (3, 1, ' ', NOW(), NOW(), NULL, NULL),
+      (4, 1, ' ', NOW(), NOW(), NULL, NULL),
+      (5, 2, ' ', NOW(), NOW(), NULL, NULL),
+      (6, 3, ' ', NOW(), NOW(), NULL, NULL),
+      (7, 3, ' ', NOW(), NOW(), NULL, NULL),
+      (8, 4, ' ', NOW(), NOW(), NULL, NULL);
