6df937b3c770859938b409f39b34b43e6239e792
Eliehoenai <rlaehdalsk@naver.com>
2024-12-20 16:55:02 +0900

    불필요한 코드 제거

--- DA-GWAN/src/main/java/hyper/darye/service/MemberService.java
+++ DA-GWAN/src/main/java/hyper/darye/service/MemberService.java
@@ -2,13 +2,11 @@ package hyper.darye.service;
 
 import hyper.darye.dto.Member;
 import hyper.darye.dto.SignUp;
-import hyper.darye.dto.controller.request.CreateMemberRequest;
 import hyper.darye.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.NoSuchElementException;
 
 @Service
@@ -19,23 +17,6 @@ public class MemberService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-//    /**
-//     * 회원 가입 로직 (개별 필드)
-//     */
-//    public int insertMember(String email, String password, String rePassword, String name, Character sex, Date birthdate, String mobile) {
-//        CreateMemberRequest member = new CreateMemberRequest();
-//        member.setEmail(email);
-//
-//        // 비밀번호 암호화 처리
-//        member.setPassword(passwordEncoder.encode(password));
-//
-//        member.setName(name);
-//        member.setSex(sex);
-//        member.setBirthdate(birthdate);
-//        member.setMobile(mobile);
-//        return memberMapper.insertMember(member);
-//    }
 
     public Member selectMemberByEmail(String email) throws NoSuchElementException {
         Member result = memberMapper.selectByEmail(email);
