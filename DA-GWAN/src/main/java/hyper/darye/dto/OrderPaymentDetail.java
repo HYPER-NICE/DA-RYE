f6f7d36fa770b633feda92331db727e9753556a2
greenyeast <djaks65@gmail.com>
2024-12-23 13:23:11 +0900

    Chore: DTO 주석 수정

--- DA-GWAN/src/main/java/hyper/darye/dto/OrderPaymentDetail.java
+++ DA-GWAN/src/main/java/hyper/darye/dto/OrderPaymentDetail.java
@@ -1,6 +1,7 @@
 package hyper.darye.dto;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Data;
 
@@ -10,45 +11,13 @@ import lombok.Data;
  */
 @Data
 public class OrderPaymentDetail extends OrderPaymentDetailKey implements Serializable {
-    /**
-     * 승인 번호
-     */
     private String approvalNumber;
-
-    /**
-     * 결제 금액
-     */
     private Integer paymentAmount;
-
-    /**
-     * 결제 날짜 및 시간
-     */
-    private Date paymentDate;
-
-    /**
-     * 결제에 대한 추가 설명
-     */
+    private LocalDateTime paymentDate;
     private String paymentComment;
-
-    /**
-     * 레코드 생성 날짜 및 시간
-     */
-    private Date createdDate;
-
-    /**
-     * 마지막 수정된 날짜 및 시간
-     */
-    private Date lastModifiedDate;
-
-    /**
-     * 레코드를 마지막으로 수정한 회원 ID
-     */
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
     private Long lastModifiedMember;
-
-    /**
-     * 레코드가 삭제된 날짜 (논리 삭제)
-     */
-    private Date deletedDate;
-
+    private LocalDateTime deletedDate;
     private static final long serialVersionUID = 1L;
 }
