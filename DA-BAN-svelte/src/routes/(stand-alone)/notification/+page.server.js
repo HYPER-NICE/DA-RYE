40a1b12067e5596d9f82f201fd4137af8cfb9183
XIYO <bunny@xiyo.dev>
2024-12-27 22:35:25 +0900

    :sparkles: 공지사항 화면 추가

--- DA-BAN-svelte/src/routes/(stand-alone)/notification/+page.server.js
+++ DA-BAN-svelte/src/routes/(stand-alone)/notification/+page.server.js
@@ -1,34 +1,59 @@
 import { SECRET_BACKEND_HOST } from '$env/static/private';
 
+const mockBoardData = [{
+	id: 1,
+	parentId: null,
+	categoryId: 1,
+	categoryName: '중요',
+	writerId: 5,
+	title: '배송 지연 안내입니다.',
+	content: '과도한 물량으로 인해 배송이 지연될 예정입니다.',
+	fixed: true,
+	regDate: '2024-12-27T12:27:07', // 문자열(ISO 8601)이거나 Date 객체
+	createdDate: '2024-12-27T12:27:07',
+	lastModifiedDate: '2024-12-27T12:27:07',
+	lastModifiedMember: null,
+	deletedDate: null
+}, {
+	id: 2,
+	parentId: null,
+	categoryId: 2,
+	categoryName: '일반',
+	writerId: 5,
+	title: '용량 변경 안내입니다.',
+	content: '녹차 상품의 용량이 변경될 예정입니다.',
+	fixed: false,
+	regDate: '2024-12-27T12:27:07',
+	createdDate: '2024-12-27T12:27:07',
+	lastModifiedDate: '2024-12-27T12:27:07',
+	lastModifiedMember: null,
+	deletedDate: null
+}];
+
+
 const pageData = {
-	title: '공지사항',
-}
+	title: '공지사항'
+};
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load({cookies}) {
+export async function load() {
+	let boards;
+	try {
+		const response = await fetch(`${SECRET_BACKEND_HOST}/api/notification-board`, {
+			method: 'GET', headers: { 'Content-Type': 'application/json' }
+		});
 
-	const jsessionId = cookies.get('JSESSIONID') ?? ''; // 없는 경우 대비
-	const cookieHeader = `JSESSIONID=${jsessionId}`;
-
-	const response = await fetch(`${SECRET_BACKEND_HOST}/api/notification-board`, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json',
-			'Cookie': {cookieHeader}
-		},
-		credentials: 'include',
-	});
-
-	console.log(response);
-	if (!response.ok) {
-		return {
-			...pageData,
-			status: response.status,
-			error: await response.text()
-		};
+		boards = await response.json();
+	} catch (error) {
+		boards = mockBoardData.map((board) => ({
+			...board, regDateFormatted: new Intl.DateTimeFormat('ko-KR', {
+				year: 'numeric', month: '2-digit', day: '2-digit'
+			}).format(new Date(board.regDate)) // regDate를 포맷
+		}));
 	}
 
 	return {
-		...pageData,
+		title: '공지사항', boards
 	};
 }
 
@@ -36,4 +61,4 @@ export const actions = {
 	default: async () => {
 
 	}
-}
+};
--- DA-BAN-svelte/src/routes/(stand-alone)/notification/+page.svelte
+++ DA-BAN-svelte/src/routes/(stand-alone)/notification/+page.svelte
@@ -1,17 +1,30 @@
 <script>
+	const { data } = $props();
+	const boards = data.boards;
 </script>
 
-{#snippet thread(data)}
-	<div>중요</div>
-	<div>2024.12.21</div>
-	<div>연휴 배송 지연 안내</div>
+{#snippet thread(board)}
+	<a href={`/notification/${board.id}`} class="flex flex-col gap-4 border-b py-6">
+		<div class="flex gap-2">
+			<div
+				class={`px-6 py-1 rounded-full border ${board.categoryId === 1 && "border-red-500 text-red-500 text-lg" } font-medium`}>{board.categoryName}</div>
+			<div class="content-center text-gray-500">{board.regDateFormatted}</div>
+		</div>
+		<div class="text-2xl">{board.title}</div>
+	</a>
 {/snippet}
 
-<!-- filter -->
-<form>
+<div class="p-4">
+	<!-- filter -->
+	<form>
 
-</form>
+	</form>
 
-<!-- content -->
+	<!-- content -->
 
-{@render thread(data)}
+	<div >
+	{#each boards as board}
+		{@render thread(board)}
+	{/each}
+	</div>
+</div>
