3a879ed6b9a919e4802af904646b89f441c73553
XIYO <bunny@xiyo.dev>
2024-12-28 22:12:58 +0900

    :lipstick: 에러 페이지 추가

---
+++ DA-BAN-svelte/src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {page} from '$app/state';
+	import done from '$assets/done.svg';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		setTimeout(() => {
+			goto('/', { replaceState: true });
+		}, 5000);
+	});
+</script>
+
+<div class="h-[600px] flex flex-col gap-2  justify-center">
+
+	<!--	icon -->
+	<div class="flex flex-col gap-6">
+		<img
+			class="mx-auto"
+			src={done}>
+		<div class="text-center text-red-500 font-semibold">
+			{page.status}
+		</div>
+	</div>
+
+	<div class="text-center font-bold text-2xl">
+		{page.error.message}
+	</div>
+
+	<div>
+		{page.error.code}
+	</div>
+
+	<div class="font-light text-sm text-center">조금 뒤에 이동합니다.</div>
+</div>
