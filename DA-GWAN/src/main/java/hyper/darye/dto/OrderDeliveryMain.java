a8c1f79f2377812baa151546db30363bd2fc3c94
XIYO <bunny@xiyo.dev>
2025-01-04 09:48:35 +0900

    :sparkles: 엔티티 생성 메서드 추가

--- DA-GWAN/src/main/java/hyper/darye/dto/OrderDeliveryMain.java
+++ DA-GWAN/src/main/java/hyper/darye/dto/OrderDeliveryMain.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 주문의 배송 정보를 저장하는 테이블로, 주문의 배송 정보를 관리합니다.
  * ORDER_DELIVERY_MAIN
  */
-@Data
+@Getter
+@NoArgsConstructor
 public class OrderDeliveryMain implements Serializable {
     private Long id;
     private Long orderMainId;                           // FK
@@ -21,4 +24,13 @@ public class OrderDeliveryMain implements Serializable {
     private Long lastModifiedMember;
     private LocalDateTime deletedDate;
     private static final long serialVersionUID = 1L;
+
+    public static OrderDeliveryMain createForAdd(Long orderId, String deliveryCompanyName, String trackingNumber, String deliveryRequestNote) {
+        OrderDeliveryMain orderDeliveryMain = new OrderDeliveryMain();
+        orderDeliveryMain.orderMainId = orderId;
+        orderDeliveryMain.deliveryCompanyName = deliveryCompanyName;
+        orderDeliveryMain.trackingNumber = trackingNumber;
+        orderDeliveryMain.deliveryRequestNote = deliveryRequestNote;
+        return orderDeliveryMain;
+    }
 }
--- DA-GWAN/src/main/java/hyper/darye/service/order/OrderService.java
+++ DA-GWAN/src/main/java/hyper/darye/service/order/OrderService.java
@@ -21,9 +21,13 @@ public class OrderService {
     private final OrderMainMapper orderMainMapper;
     private final OrderDetailMapper orderDetailMapper;
 
+//     TODO 프로덕트 키 검증이 필요한가? 필요하다면 무슨 에러를 던질것인가?
+
     /**
-     * 주문 생성
-     * TODO 프로덕트 키 검증이 필요한가? 필요하다면 무슨 에러를 던질것인가?
+     * 주문 생성합니다. 메인 테이블, 디테일 테이블에 데이터가 모두 생성됩니다.
+     * @param memberId
+     * @param products
+     * @return
      */
     @Transactional
     public int add(Long memberId, List<Product> products) {
@@ -35,6 +39,10 @@ public class OrderService {
         List<OrderDetail> orderDetails = new ArrayList<>();
         for(Product product : products) {
             OrderDetail orderDetail = OrderDetail.createForAdd(orderMain.getId(), product);
+
+            // TODO 주문된 물건의 수량을 차감하는 기능
+            // TODO 배송 상태 메서드 추가
+            // TODO 결제 상태 메서드 추가
             orderDetails.add(orderDetail);
         }
         return orderDetailMapper.insertBulk(orderDetails);
