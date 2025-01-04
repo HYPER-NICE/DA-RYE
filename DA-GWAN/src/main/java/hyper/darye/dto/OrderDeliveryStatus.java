49534629af9145287425635857018374fab04650
XIYO <bunny@xiyo.dev>
2025-01-04 10:00:14 +0900

    :sparkles: 오더 딜리버리 스테이터스 변경

--- DA-GWAN/src/main/java/hyper/darye/dto/OrderDeliveryStatus.java
+++ DA-GWAN/src/main/java/hyper/darye/dto/OrderDeliveryStatus.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 주문의 배송 상태 변경 이력을 저장하는 테이블로, 각 주문의 배송 상태 변화를 추적합니다.
  * ORDER_DELIVERY_STATUS
  */
-@Data
+@Getter
+@NoArgsConstructor
 public class OrderDeliveryStatus implements Serializable {
 
     private Long id;
@@ -21,4 +24,12 @@ public class OrderDeliveryStatus implements Serializable {
     private Long lastModifiedMember;
     private LocalDateTime deletedDate;
     private static final long serialVersionUID = 1L;
+
+    public static OrderDeliveryStatus createForAdd(Long orderDeliveryMainId, Long deliveryStatusCodeId, LocalDateTime deliveryDate) {
+        OrderDeliveryStatus orderDeliveryStatus = new OrderDeliveryStatus();
+        orderDeliveryStatus.orderDeliveryMainId = orderDeliveryMainId;
+        orderDeliveryStatus.deliveryStatusCodeId = deliveryStatusCodeId;
+        orderDeliveryStatus.deliveryDate = deliveryDate;
+        return orderDeliveryStatus;
+    }
 }
